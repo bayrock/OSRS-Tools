8fce043281623ed56657d20bfcfacca172629439
Bailey Chestnut <bayrock@devinity.org>
2020-12-20 13:51:13 -0500

    Dynamically load patch selection from JSON data

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
+
+let patchData
 function calculatePetChance() {
     // return the chance to the end user
 }
 
-function addSelections() {
-    // add the patches to the selection list
+function addPatches(patches) {
+    const selector = document.getElementById("patchSelector")
+
+    let option
+    patches.forEach(entry => {
+        option = document.createElement("option")
+        option.text = entry.produce
+        option.value = entry.produce.toLowerCase()
+
+        selector.add(option)
+    });
 }
 
 $.getJSON("patches.json", (data) => {
-    console.log(data)
+    patchData = data
+    addPatches(data)
 })
