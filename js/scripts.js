00bdbc1b4b51b7499efd783ba0e04aaa58cb28f1
Bailey Chestnut <bayrock@devinity.org>
2020-12-20 11:20:13 -0500

    Preparing boilerplate

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function calculatePetChance() {
+    // return the chance to the end user
+}
+
+function addSelections() {
+    // add the patches to the selection list
+}
+
+$.getJSON("patches.json", (data) => {
+    console.log(data)
+})
