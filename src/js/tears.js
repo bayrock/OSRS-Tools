da4a71b8127067dfe6eae4a71eeba96013e564c6
Bailey Chestnut <bayrock@devinity.org>
2020-12-23 01:26:35 -0500

    Refine Tears of Guthix Countdown page

--- src/js/tears.js
+++ src/js/tears.js
@@ -1,19 +1,20 @@
 
-const wednesday = 3
 function getDelta() {
     const now = new Date()
+    now.setHours(now.getHours() + 5) // EST to UTC compensation
     const day = now.getDay()
     const hours = 23 - now.getHours()
     const minutes = 60 - now.getMinutes()
     const seconds = 60 - now.getSeconds()
 
     let  days
+    const wednesday = 3
     if (day < wednesday)
         days = wednesday - (day + 1)
     else if (day > wednesday)
-        days = day + (wednesday - 1)
+        days = 6 - (day - wednesday)
     else
-        days = 0 // it's Wednesday my dudes!
+        days = 6 // it's Wednesday my dudes!
 
     return [getPlural("day", days),
             getPlural("hour", hours),
@@ -22,5 +23,5 @@ function getDelta() {
 }
 
 $(document).ready(() => {
-    $("#clock").html(`${getDelta()}<br><i>left until reset</i>`)
+    $("#clock").html(`<b>${getDelta()}</b><br><span class="lightgrey"><i>until reset (Wednesday 00:00 UTC)</i></span>`)
 })
--- src/js/util.js
+++ src/js/util.js
@@ -3,4 +3,4 @@ function setError(str) {resetAlert(); $("#errorBox").html(str)}
 function resetAlert() {$("#alertBox").html("")}
 function resetError() {$("#errorBox").html("")}
 
-function getPlural(str, int) {return int > 0 ? int + " " + (int > 1 ? str + "s" : str) : ""}
+function getPlural(str, int) {return int + " " + (int > 1 ? str + "s" : str)}
